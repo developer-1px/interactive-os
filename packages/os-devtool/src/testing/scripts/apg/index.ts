d4ee6e4e8e8e7f045e52308f2920af809b6b030f
유용태 <teo.v@navercorp.com>
2026-03-04 01:08:08 +0900

    style: sort APG scripts index alphabetically

--- packages/os-devtool/src/testing/scripts/apg/index.ts
+++ packages/os-devtool/src/testing/scripts/apg/index.ts
@@ -3,81 +3,85 @@
  *
  * Each pattern has its own file for maintainability.
  * This index re-exports individual scripts and the combined bundle.
+ * Sorted alphabetically.
  */
 
 import type { TestScript } from "../../types";
 
-export { apgListboxSingleScript } from "./listbox-single";
-export { apgListboxMultiScript } from "./listbox-multi";
-export { apgTabsAutoScript } from "./tabs";
-export { apgTreeScript } from "./tree";
-export { apgToolbarScript } from "./toolbar";
+// ─── Re-exports (alphabetical) ───
+
 export { apgAccordionScript } from "./accordion";
-export { apgMenuScript } from "./menu";
-export { apgGridScript } from "./grid";
-export { apgSwitchScript } from "./switch";
-export { apgSliderScript } from "./slider";
-export { apgRadiogroupScript } from "./radiogroup";
+export { apgButtonScript } from "./button";
+export { apgCarouselScript } from "./carousel";
 export { apgCheckboxScript } from "./checkbox";
 export { apgDisclosureScript } from "./disclosure";
-export { apgTooltipScript } from "./tooltip";
-export { apgButtonScript } from "./button";
 export { apgFeedScript } from "./feed";
-export { apgCarouselScript } from "./carousel";
+export { apgGridScript } from "./grid";
+export { apgListboxMultiScript } from "./listbox-multi";
+export { apgListboxSingleScript } from "./listbox-single";
+export { apgMenuScript } from "./menu";
 export { apgMenuButtonScript } from "./menu-button";
 export { apgMeterScript } from "./meter";
-export { apgSpinbuttonScript } from "./spinbutton";
+export { apgRadiogroupScript } from "./radiogroup";
+export { apgSliderScript } from "./slider";
 export { apgSliderMultiThumbScript } from "./slider-multithumb";
+export { apgSpinbuttonScript } from "./spinbutton";
+export { apgSwitchScript } from "./switch";
+export { apgTabsAutoScript } from "./tabs";
+export { apgToolbarScript } from "./toolbar";
+export { apgTooltipScript } from "./tooltip";
+export { apgTreeScript } from "./tree";
 export { apgTreegridScript } from "./treegrid";
 export { apgWindowSplitterScript } from "./window-splitter";
 
-// Individual imports for bundle array
-import { apgListboxSingleScript } from "./listbox-single";
-import { apgListboxMultiScript } from "./listbox-multi";
-import { apgTabsAutoScript } from "./tabs";
-import { apgTreeScript } from "./tree";
-import { apgToolbarScript } from "./toolbar";
+// ─── Bundle imports (alphabetical) ───
+
 import { apgAccordionScript } from "./accordion";
-import { apgMenuScript } from "./menu";
-import { apgGridScript } from "./grid";
-import { apgSwitchScript } from "./switch";
-import { apgSliderScript } from "./slider";
-import { apgRadiogroupScript } from "./radiogroup";
+import { apgButtonScript } from "./button";
+import { apgCarouselScript } from "./carousel";
 import { apgCheckboxScript } from "./checkbox";
 import { apgDisclosureScript } from "./disclosure";
-import { apgTooltipScript } from "./tooltip";
-import { apgButtonScript } from "./button";
 import { apgFeedScript } from "./feed";
-import { apgCarouselScript } from "./carousel";
+import { apgGridScript } from "./grid";
+import { apgListboxMultiScript } from "./listbox-multi";
+import { apgListboxSingleScript } from "./listbox-single";
+import { apgMenuScript } from "./menu";
 import { apgMenuButtonScript } from "./menu-button";
 import { apgMeterScript } from "./meter";
-import { apgSpinbuttonScript } from "./spinbutton";
+import { apgRadiogroupScript } from "./radiogroup";
+import { apgSliderScript } from "./slider";
 import { apgSliderMultiThumbScript } from "./slider-multithumb";
+import { apgSpinbuttonScript } from "./spinbutton";
+import { apgSwitchScript } from "./switch";
+import { apgTabsAutoScript } from "./tabs";
+import { apgToolbarScript } from "./toolbar";
+import { apgTooltipScript } from "./tooltip";
+import { apgTreeScript } from "./tree";
 import { apgTreegridScript } from "./treegrid";
 import { apgWindowSplitterScript } from "./window-splitter";
 
 export const apgShowcaseScripts: TestScript[] = [
-    apgListboxSingleScript,
-    apgListboxMultiScript,
-    apgTabsAutoScript,
-    apgTreeScript,
-    apgToolbarScript,
     apgAccordionScript,
-    apgMenuScript,
-    apgGridScript,
-    apgSwitchScript,
-    apgSliderScript,
-    apgRadiogroupScript,
+    apgButtonScript,
+    apgCarouselScript,
     apgCheckboxScript,
     apgDisclosureScript,
-    apgTooltipScript,
-    apgButtonScript,
     apgFeedScript,
-    apgCarouselScript,
+    apgGridScript,
+    apgListboxMultiScript,
+    apgListboxSingleScript,
+    apgMenuScript,
     apgMenuButtonScript,
     apgMeterScript,
-    apgSpinbuttonScript,
+    apgRadiogroupScript,
+    apgSliderScript,
     apgSliderMultiThumbScript,
+    apgSpinbuttonScript,
+    apgSwitchScript,
+    apgTabsAutoScript,
+    apgToolbarScript,
+    apgTooltipScript,
+    apgTreeScript,
     apgTreegridScript,
     apgWindowSplitterScript,
 ];
